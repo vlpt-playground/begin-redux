349a6aae083547317279b19157153c21aea71761
velopert <public.velopert@gmail.com>
2018-01-01 13:01:51 +0900

    refactored counter

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Counter from 'components/Counter';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as counterActions from 'store/modules/counter';
 
 class CounterContainer extends Component {
   handleIncrement = () => {
-    this.props.increment();
+    const { CouonterActions } = this.props;
+    CouonterActions.increment();
   }
-
   handleDecrement = () => {
-    this.props.decrement();
+    const { CouonterActions } = this.props;
+    CouonterActions.decrement();
   }
   render() {
     const { handleIncrement, handleDecrement } = this;
@@ -25,18 +27,17 @@ class CounterContainer extends Component {
   }
 }
 
-// props 값으로 넣어 줄 상태를 정의해줍니다.
-const mapStateToProps = (state) => ({
-  number: state.counter.number
-});
+/* 첫번째 파라미터 mapStateToProps: props 값으로 넣어 줄 상태를 정의해줍니다.
+   두번째 파라미터 mapDispatchToProps: props 값으로 넣어 줄 액션 함수들을 정의해줍니다
 
-// props 값으로 넣어 줄 액션 함수들을 정의해줍니다
-const mapDispatchToProps = (dispatch) => ({
-  increment: () => dispatch(counterActions.increment()),
-  decrement: () => dispatch(counterActions.decrement())
-})
-
-// 컴포넌트를 리덕스와 연동 할 떄에는 connect 를 사용합니다.
-// connect() 의 결과는, 컴포넌트에 props 를 넣어주는 함수를 반환합니다.
-// 반환된 함수에 우리가 만든 컴포넌트를 넣어주면 됩니다.
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+   컴포넌트를 리덕스와 연동 할 떄에는 connect 를 사용합니다.
+   connect() 의 결과는, 컴포넌트에 props 를 넣어주는 함수를 반환합니다.
+   반환된 함수에 우리가 만든 컴포넌트를 넣어주면 됩니다. */
+export default connect(
+  (state) => ({
+    number: state.counter.number
+  }), 
+  (dispatch) => ({
+    CouonterActions: bindActionCreators(counterActions, dispatch)
+  })
+)(CounterContainer);
