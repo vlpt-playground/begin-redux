dfa7c39e05857ade3107d299927aa963af9c799e
velopert <public.velopert@gmail.com>
2018-04-14 05:02:48 +0900

    applied devtools

--- src/components/AppTemplate.js
+++ src/components/AppTemplate.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './AppTemplate.css';
+import DevTools from 'mobx-react-devtools';
 
 const AppTemplate = ({counter, todos}) => {
   return (
     <div className="app-template">
       <div className="counter">{counter}</div>
       <div className="todos">{todos}</div>
+      <DevTools />
     </div>
   );
 };
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,60 +1,56 @@
-import React from 'react';
-import { List, Map } from 'immutable';
+import React from "react";
+import { observer } from "mobx-react";
 
 const TodoItem = ({ id, text, checked, onToggle, onRemove }) => (
-  <li 
+  <li
     style={{
-      textDecoration: checked ? 'line-through' : 'none'
-    }} 
+      textDecoration: checked ? "line-through" : "none"
+    }}
     onClick={() => onToggle(id)}
-    onDoubleClick={() => onRemove(id)}>
+    onDoubleClick={() => onRemove(id)}
+  >
     {text}
   </li>
-)
+);
 
-const Todos = ({todos, input, onInsert, onToggle, onRemove, onChange }) => {
-  
-  const todoItems = todos.map(
-    todo => {
-      const { id, checked, text } = todo.toJS();
-      return (
-        <TodoItem
-          id={id}
-          checked={checked}
-          text={text}
-          onToggle={onToggle}
-          onRemove={onRemove}
-          key={id}
-        />
-      )
-    }
-  )
+const Todos = ({ todos, input, onInsert, onToggle, onRemove, onChange }) => {
+  const todoItems = todos.map(todo => {
+    const { id, text, checked } = todo;
+    return (
+      <TodoItem
+        id={id}
+        checked={checked}
+        text={text}
+        onToggle={onToggle}
+        onRemove={onRemove}
+        key={id}
+      />
+    );
+  });
   return (
     <div>
       <h2>오늘 할 일</h2>
-      <input value={input} onChange={onChange}/>
+      <input value={input} onChange={onChange} />
       <button onClick={onInsert}>추가</button>
-      <ul>
-        { todoItems }
-      </ul>
+      <ul>{todoItems}</ul>
     </div>
   );
 };
 
 Todos.defaultProps = {
-  todos: List([
-    Map({
+  todos: [
+    {
       id: 0,
-      text: '걷기',
+      text: "걷기",
       checked: false
-    }),
-    Map({
+    },
+    {
       id: 1,
-      text: '코딩하기',
+      text: "코딩하기",
       checked: true
-    })
-  ]),
-  input: ''
+    }
+  ],
+  input: ""
 };
 
-export default Todos;
+export default observer(Todos);
